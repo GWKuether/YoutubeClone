f739ebd956af687a2704d97c769179090cbc7269
George Kuether <gwkuether@gmail.com>
2022-12-22 10:55:07 -0600

    Added Functionality that Allows User to Update Comment

--- backend/Comments/urls.py
+++ backend/Comments/urls.py
@@ -4,7 +4,7 @@ from Comments import views
 urlpatterns = [
     path('', views.user_comments),
     path('<str:video_id>/', views.get_video_id_comments),
-    path('<int:pk>/', views.comment_detail)
+    path('detail/<int:pk>/', views.comment_detail)
 
 ]
 
